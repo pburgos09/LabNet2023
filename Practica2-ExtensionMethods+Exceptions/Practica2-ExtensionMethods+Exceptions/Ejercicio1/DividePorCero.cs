27905ab0965a57ffef35077bb3a48e4fd553fed6
pburgos09 <burgospablom@gmail.com>
2023-08-22 21:53:22 -0300

    Se sube codigo de Ejercicio 1
    Se sube codigo resuelto de ejercicio 1

---
+++ Practica2-ExtensionMethods+Exceptions/Practica2-ExtensionMethods+Exceptions/Ejercicio1/DividePorCero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2_ExtensionMethods_Exceptions.Ejercicio1
+{
+    public static class DividePorCero
+    {
+        public static void DividirPorCero()
+        {
+            try
+            {
+                Console.WriteLine("Ejercicio 1: Divide por cero");
+                Console.WriteLine("Ingrese el dividendo: ");
+                int dividendo = int.Parse(Console.ReadLine());
+                int divisor = 0;
+                int resultado = dividendo / divisor;
+                Console.WriteLine($"El resultado de la division es: {resultado}");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("No se puede dividir por cero");
+                Console.WriteLine($"Mensaje de la excepcion: {e.Message}");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("El valor ingresado no es un numero");
+                Console.WriteLine($"Mensaje de la excepcion: {e.Message}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Mensaje de la excepcion: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Ejercicio finalizado.");
+            }
+        }
+    }
+}
---
+++ Practica2-ExtensionMethods+Exceptions/Practica2-ExtensionMethods+Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practica2_ExtensionMethods_Exceptions.Ejercicio1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2_ExtensionMethods_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DividePorCero.DividirPorCero();
+            Console.ReadKey();
+        }
+    }
+}
